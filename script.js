d7a13ea4968fa3a7873e6d4aed35e9520ce3e881
Lucia <lcm471@nyu.edu>
2022-12-13 09:24:22 -0500

    i am saving

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
-const day = new Date();
-d = day.getDate();
-document.getElementById("date").innerHTML = d;
+
+const getter = new Date();
+
+d = getter.getDate();
+day = getter.getDay();
+
+if (day == 0){
+  // Sunday
+  let s = getElementById("sun")
+  s.textContent = 1;
+}
+if (day == 1){
+  // Monday
+
+}
+if (day == 2){
+  // tuesday
+
+
+}
+if (day == 3){
+  // wednesday
+
+}
+if (day == 4){
+  // thu
+
+}
+if (day == 5){
+  // fri
+
+}
+if (day == 6){
+  // sat
+
+}
